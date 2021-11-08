61d5ab135f3ae6bd2f55412343617c06a46d50e2
AleksandrTsarik <ascwork86@gmail.com>
2021-11-08 22:25:44 +0500

    add for + if

--- common.js
+++ common.js
@@ -9,12 +9,29 @@ const personalMoveDB = {
    geners: [],
    private: false,
 };
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = +prompt('на сколько Вы его оцените?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = +prompt('на сколько Вы его оцените?', '');
 
-personalMoveDB.movise[a] = b;
-personalMoveDB.movise[c] = d;
+// for(let i = 0; i < 2; i++){
+//    const a = prompt('Один из последних просмотренных фильмов?', ''),
+//          b = +prompt('на сколько Вы его оцените?', '');
 
+//          personalMoveDB.movise[a] = b;
+
+//    if (a.length < 50 && a != null && b!= null && a != '' && b!= ''){
+//       personalMoveDB.movise[a] = b;
+//       console.log('done');
+//    } else {
+//       console.log('error');
+//       i--;
+//    }
+// }
 console.log(personalMoveDB);
+
+   if (personalMoveDB.count < 10 ) {
+      alert('Просмотренно довольно мало фильмов');
+   } else if ( personalMoveDB.count >= 10 && personalMoveDB.count < 30) {
+      alert('Вы классический зритель');
+   } else if(personalMoveDB.count >= 30 ){
+      alert('Вы киноман');
+   }else{
+      alert('Произошла ошибка')
+   };
