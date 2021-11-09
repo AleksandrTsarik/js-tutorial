11f67e0b4f4c8a0add6fdc6570c90373b5e3f1f7
AleksandrTsarik <ascwork86@gmail.com>
2021-11-09 21:30:56 +0500

    test #3

--- common.js
+++ common.js
@@ -1,7 +1,17 @@
 'use strict'
 
 
-const numberOfFilms = +prompt('Сколько фильмов Вы уже просмотрели?', '');
+let numberOfFilms
+
+function start() {
+   numberOfFilms = +prompt('Сколько фильмов Вы уже просмотрели?', '');
+
+   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) { // проверка поля на ввод данных
+      numberOfFilms = +prompt('Сколько фильмов Вы уже просмотрели?', '');
+   }
+};
+start();
+
 const personalMoveDB = {
    count: numberOfFilms,
    movise: {},
@@ -9,23 +19,26 @@ const personalMoveDB = {
    geners: [],
    private: false,
 };
-
-for(let i = 0; i < 2; i++){
-   const a = prompt('Один из последних просмотренных фильмов?', ''),
-         b = +prompt('на сколько Вы его оцените?', '');
-
-         personalMoveDB.movise[a] = b;
-
-   if (a.length < 50 && a != null && b!= null && a != '' && b!= ''){
-      personalMoveDB.movise[a] = b;
-      console.log('done');
-   } else {
-      console.log('error');
-      i--;
-   }
-}
 console.log(personalMoveDB);
 
+function rememberMyFilms() {
+   for(let i = 0; i < 2; i++){
+      const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = +prompt('на сколько Вы его оцените?', '');
+            personalMoveDB.movise[a] = b;
+   
+      if (a.length < 50 && a != null && b!= null && a != '' && b!= ''){
+         personalMoveDB.movise[a] = b;
+         console.log('done');
+      } else {
+         console.log('error');
+         i--;
+      }
+   }
+};
+//rememberMyFilms();
+
+function detectPersonalLevel() {
    if (personalMoveDB.count < 10 && personalMoveDB.count != null && personalMoveDB.count != '' ) {
       alert('Просмотренно довольно мало фильмов');
    } else if ( personalMoveDB.count >= 10 && personalMoveDB.count < 30) {
@@ -35,3 +48,22 @@ console.log(personalMoveDB);
    }else{
       alert('Произошла ошибка')
    };
+};
+//detectPersonalLevel();
+
+function showMyDB(hidden) {
+   if(!hidden){
+      console.log(personalMoveDB)
+   }else{
+      console.log('ЗАкрыто ')
+   }
+}
+showMyDB(personalMoveDB.private);
+
+function writeYourGenres() {
+   for(let i = 1; i <= 3; i++) {
+      const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+      personalMoveDB.geners[i - 1] = genre;
+   }
+}
+writeYourGenres();
